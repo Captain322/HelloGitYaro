9ec83e6c157d8b8475570b09af267cb25693518d
Yaroslav Mikhnevych <steamtrades68@gmail.com>
2020-09-04 16:52:22 -0400

    added variables to the stat checker

---
+++ Variable stat check/Variable stat check/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Variable_stat_check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = ("Captain322");
+            const int StartingPoints = 50;
+            int Strength = StartingPoints / 10 * 2;
+            int Dexterity = Strength / 2;
+            int Intelligence = 7;
+            int Health = Dexterity + Intelligence - 2;
+            int Charisma = StartingPoints - Strength - Dexterity - Intelligence - Health;
+            int TotalPoints;
+            TotalPoints = Strength + Dexterity + Intelligence + Health + Charisma;
+            Console.WriteLine("Hello {0}", Name);
+            Console.WriteLine("Welcome to the stat checker 2.0");
+            Console.WriteLine("Your Strength is {0}", Strength);
+            Console.WriteLine("Your Dexterity is {0}", Strength);
+            Console.WriteLine("Your Intelligence is {0}", Strength);
+            Console.WriteLine("Your Health is {0}", Strength);
+            Console.WriteLine("Your Charisma is {0}", Strength);
+            Console.WriteLine("Your build is complete, your total number of points is - {0}", + TotalPoints);
+        }
+    }
+}
