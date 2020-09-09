3278f993e6ad4b0d81d91ddef90c9cd41b091b10
Yaroslav Mikhnevych <steamtrades68@gmail.com>
2020-09-09 16:46:11 -0400

    In order to run the code the program needs to boot without debugging - important note

---
+++ Input and strings PE/Input and strings PE/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input_and_strings_PE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Enter your pet's name: ");
+            string PetName = Console.ReadLine();
+
+            Console.WriteLine("Enter your country of origin: ");
+            string Country = Console.ReadLine();
+
+            Console.WriteLine("Enter your favorite color: ");
+            string Color = Console.ReadLine();
+
+            Console.WriteLine("Your name has " + (Name.Length) + " characters");
+
+            Console.WriteLine("Your name is " + ((Name.Length) - (PetName.Length)) + " character longer than your pet's name");
+
+            Console.WriteLine("So you're gonna tell me that your " + Color + " pet " + PetName + " was born in " + Country + "?");
+
+            Console.WriteLine("You know, your name in French would probably be spelled " + (Name.ToUpper()[0] + PetName.Substring(0,2) + Country.Substring(0,2) + Color.Substring(0,2)));
+        }
+    }
+}
---
+++ PE Zip Files/PE Zip Files/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PE_Zip_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zip it and ship it");
+            Console.WriteLine("My name is Yaroslav Mikhnevych :)");
+        }
+    }
+}
