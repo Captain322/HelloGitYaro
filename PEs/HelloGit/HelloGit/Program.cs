10b2f8f7f2d87138d11e7b085f56500be600a496
Yaroslav Mikhnevych <steamtrades68@gmail.com>
2020-09-14 14:38:32 -0400

    Updated all the old folders into a new repo

---
+++ PEs/HelloGit/HelloGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloGit
+{
+    class Program
+    {
+        private static bool name;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi!");
+            Console.ReadLine();
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ Stat Check/Stat Check/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Stat_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            Console.WriteLine("What's your name?");
+            Name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", Name + " welcome to the stat checker");
+            Console.WriteLine("Let's get you some stats - you have 50 points total");
+            Console.WriteLine("Current stats: ");
+            Console.WriteLine("Strength - " + (50 * 0.2));
+            Console.WriteLine("Perception - " + (50 * 0.2 * 0.5));
+            Console.WriteLine("Endurance - 7");
+            Console.WriteLine("Charisma - " + (5 + 7 - 2));
+            Console.WriteLine("Intellegence - " + (50 - 10 - 5 - 7 - 10));
+            Console.WriteLine("Press Alt + F4 to re-distribute");
+
+        }
+    }
+}
